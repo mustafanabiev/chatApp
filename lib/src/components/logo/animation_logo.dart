1359482c5a8337b57161c15799493375ed3fa78d
mustafanabiev <nabievvv02@gmail.com>
2023-03-13 23:56:41 +0600

    integration firebase

---
+++ lib/src/components/logo/animation_logo.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class AnimationLogo extends StatefulWidget {
+  const AnimationLogo({super.key, this.size = 63});
+  final double size;
+
+  @override
+  State<AnimationLogo> createState() => _AnimationLogoState();
+}
+
+class _AnimationLogoState extends State<AnimationLogo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
+
+    controller.forward();
+
+    animation.addStatusListener((status) {
+      print(status);
+    });
+
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Hero(
+      tag: 'logo',
+      child: SizedBox(
+        height: controller.value * widget.size,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+  }
+}
