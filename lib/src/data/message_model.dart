64f36a5eb0ad550bd008be10c5407bdd70b3be4d
mustafanabiev <nabievvv02@gmail.com>
2023-03-28 01:34:31 +0600

    chat app buttu

--- lib/src/data/message_model.dart
+++ lib/src/data/message_model.dart
@@ -1,16 +1,22 @@
 class Message {
-  const Message({
+  Message({
     required this.sender,
     required this.sms,
+    required this.dateTime,
+    this.isMy,
   });
 
   final String sender;
   final String sms;
+  final DateTime dateTime;
+  bool? isMy;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'sender': sender,
       'sms': sms,
+      'dateTime': dateTime.millisecondsSinceEpoch,
+      'isMy': isMy,
     };
   }
 
@@ -18,6 +24,8 @@ class Message {
     return Message(
       sender: map['sender'] as String,
       sms: map['sms'] as String,
+      dateTime: DateTime.fromMillisecondsSinceEpoch(map['dateTime'] as int),
+      isMy: map['isMy'] != null ? map['isMy'] as bool : null,
     );
   }
 }
